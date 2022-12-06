5a6720f61cde78bc2f8c9b5ed62176f899e6501d
sbareli <basixp@gmail.com>
2022-12-06 19:17:01 +0200

    fixed POM bh and compiling

--- src/main/java/mprest/com/example/demo/Controller/ShapeController.java
+++ src/main/java/mprest/com/example/demo/Controller/ShapeController.java
@@ -1,8 +1,14 @@
 package mprest.com.example.demo.Controller;
 
+import brave.Tracer;
 import mprest.com.example.demo.Entity.RightTriangle;
 import mprest.com.example.demo.Service.ShapeService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,6 +16,11 @@ import java.util.List;
 @RestController
 @RequestMapping("/shapes")
 public class ShapeController {
+
+	private static final Logger log = LoggerFactory.getLogger(ShapeController.class);
+
+	@Autowired
+	private Tracer fTracer;
 
     @Autowired
     private ShapeService shapeService ;
@@ -36,12 +47,12 @@ public class ShapeController {
         return shapeService.getTrianglesByState(state);
     }
 
-    @RequestMapping(value = "/{id}", method=RequestMethod.PUT, consumes = org.springframework.http.MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "/{id}", method=RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
     public RightTriangle updateTriangle(@PathVariable("id") int id, @RequestBody RightTriangle shape) {
         return shapeService.updateTriangle(id, shape);
     }
 
-    @RequestMapping(value = "/rightTriangles", method = RequestMethod.POST, consumes = org.springframework.http.MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "/rightTriangles", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
     public RightTriangle createTriangle(@RequestBody RightTriangle shape) {
         return shapeService.createTriangle(shape);
     }
